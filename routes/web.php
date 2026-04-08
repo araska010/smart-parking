8cee65ae546d361f22f75e83c64bf8fd42ee75b8
Araska Abu Mubarokah <araskalabsw@gmail.com>
2026-04-08 10:13:50 +0700

    add login redirect

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('/', '/admin/login');
+    return redirect('/', '/admin/login');
 });
 
 Route::get('/struk/{transaksi}', function ($transaksi) {
